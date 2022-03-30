f2b139b4119df384378d9689ae56cade1a23eae1
RosarioRu <mariarruval@gmail.com>
2022-03-29 19:38:36 -0700

    Clean up the code and add comment to change line 21 for the function we want ran.
    Co-authored-by: Eleanor Quandt <elquandt@gmail.com>
    Co-authored-by: Sean Keane <seankeane93@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
 
-/*function add(number1, number2) {
+function add(number1, number2) {
   return number1 + number2;
-} */
+} 
 
 function subtract(number1, number2) {
   return number1 - number2;
 }
 
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1/number2;
+}
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-alert(subtract(number1, number2));
-
+alert(subtract(number1, number2)); //Modify the code to call the function you want to use!
 
 
